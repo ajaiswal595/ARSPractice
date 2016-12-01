c807044488742e1c6fc5484a607bc7abc7e13180
ajaiswal595 <ajaiswal595@gmail.com>
2016-12-01 12:09:43 +0530

    commit maven quickstart archetype

---
+++ ARSControllers/src/main/java/com/ap/controller/ARSControllers/App.java
@@ -0,0 +1,13 @@
+package com.ap.controller.ARSControllers;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
